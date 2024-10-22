a87c71302c7c09a39efe9bc5697de1ba55ded34f
jgoodyear <jgoodyear@apache.org>
2024-10-22 10:45:54 -0230

    Add AppWar for local dev

---
+++ agentSystem/AppWar/src/main/webapp/chatroom.js
@@ -0,0 +1,55 @@
+//
+var messagesTableBody = document.getElementById('messagesTableBody');
+var thinkingRow = document.createElement('tr');
+thinkingRow.setAttribute('id', 'thinking');
+thinkingRow.innerHTML = '<td><p class=\"thinking-msg\">thinking...</p></td>' +
+    '<td></td>';
+
+function getTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    hours = hours < 10 ? '0' + hours : hours;
+    var minutes = now.getMinutes();
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var seconds = now.getSeconds();
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    var time = hours + ":" + minutes + ":" + seconds;
+    return time;
+}
+
+function sendMessage() {
+    var myMessageRow = document.createElement('tr');
+    var myMessage = document.getElementById('myMessage').value;
+    myMessageRow.innerHTML = '<td><p class=\"my-msg\">' + myMessage + '</p></td>' +
+        '<td>' + getTime() + '</td>';
+    messagesTableBody.appendChild(myMessageRow);
+    messagesTableBody.appendChild(thinkingRow);
+    document.getElementById('myMessage').value = "";
+
+    userAction(myMessage);
+}
+
+uri = "http://127.0.0.1:8181/cxf/ai/ask";
+
+// Make a POST call
+const userAction = async (myMessage) => {
+
+    const response = await fetch(uri, {
+        mode: 'cors',
+        method: 'POST',
+        body: myMessage, // string or object
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Content-Type': 'text/plain',
+            'Accept': 'text/plain'
+        }
+    });
+
+    const myReply = await response.text();
+    messagesTableBody.removeChild(thinkingRow);
+    var agentMessageRow = document.createElement('tr');
+    agentMessageRow.innerHTML = '<td><p class=\"agent-msg\">' + myReply + '</p></td>' +
+        '<td>' + getTime() + '</td>';
+    messagesTableBody.appendChild(agentMessageRow);
+}
